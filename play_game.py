233a31ef33d6aa6b9da25c15d7a71d68ab84ea79
Karo <karolina.stachowiak0@gmail.com>
2023-01-09 21:50:54 +0100

    Add feature smart computer

--- play_game.py
+++ play_game.py
@@ -2,6 +2,7 @@ from board_creation import create_the_board, create_list_of_numbers
 from player_move import mark_move_on_the_board, make_user_move, make_computer_move
 from display import print_board, annouce_the_winner, annouce_the_draw
 from check_for_winner import check_diags, check_columns_and_rows, is_game_over, is_draw  
+from smart_computer import make_smart_move
 
 def play():
     board = create_the_board()
@@ -13,7 +14,7 @@ def play():
         if is_game_over(board,numbers) or is_draw(board,numbers):
             break
         else:
-            computer_move = make_computer_move(numbers,board)
+            computer_move = make_smart_move(board, numbers)
             mark_move_on_the_board(board, computer_move, "O")
     if is_draw(board, numbers) == True:
         annouce_the_draw()
--- test.py
+++ test.py
@@ -1,24 +1,24 @@
-from display import annouce_the_winner
+from board_creation import create_the_board, create_list_of_numbers
+from player_move import mark_move_on_the_board, make_user_move, make_computer_move
+from display import print_board, annouce_the_winner, annouce_the_draw
+from check_for_winner import check_diags, check_columns_and_rows, is_game_over, is_draw  
+from smart_computer import make_smart_move
 
-
-
-def create_the_board(board_size):
-    x = int(board_size)
-    create_list_of_numbers(x)
-    board = []
-    for i in range(x):
-        row = []
-        for j in range(x):
-            row.append((((j+1)+(x*i))))
-        board.append(row)
-    
-    print(board)
-
-def create_list_of_numbers(x):
-    numbers = []
-    for i in range(x*x):
-        numbers.append(i+1)
-    print(numbers)
-
-
-create_the_board()
+def play():
+    board = create_the_board()
+    numbers = create_list_of_numbers(board)
+    print_board(board)
+    while not is_game_over(board, numbers) and not is_draw(board, numbers):
+        user_move = make_user_move(board,numbers)
+        mark_move_on_the_board(board, user_move, "X")
+        if is_game_over(board,numbers) or is_draw(board,numbers):
+            break
+        else:
+            computer_move = make_smart_move(board, numbers)
+            mark_move_on_the_board(board, computer_move, "O")
+    if is_draw(board, numbers) == True:
+        annouce_the_draw()
+    else:
+        X_or_O = check_diags(board) or check_columns_and_rows(board)
+        annouce_the_winner(X_or_O)
+play()
