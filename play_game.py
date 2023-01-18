7b5a946d0776326b43ca2033d8161e869854cb56
Tomek Giela <tomasz.giela@hotmail.com>
2023-01-18 19:53:49 +0100

    Add offensive move feature

--- play_game.py
+++ play_game.py
@@ -1,7 +1,7 @@
 from board import create_the_board, create_list_of_items_in, mark_move_on_the_board
 from player_move import  make_user_move, user_mark
 from display import print_board, annouce_the_winner, annouce_the_draw, ask_for_size_of_board
-from check_for_winner import get_winner_if_line_full, is_game_over, is_draw  
+from check_for_winner import get_winner_if_line_full, is_game_over, is_draw
 from computer_move import make_smart_move, computer_mark
 
 def play():
@@ -40,10 +40,10 @@ def play_reverse():
             user_move = make_user_move(board,numbers)
             mark_move_on_the_board(board, user_move, user_mark)
             print_board(board)
-    if is_draw(board):
-        annouce_the_draw()
-    else:
+    if get_winner_if_line_full(board):
         X_or_O = get_winner_if_line_full(board)
         annouce_the_winner(X_or_O)
+    else:
+        annouce_the_draw()
 
 play_reverse()
